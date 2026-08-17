77467ba07cbb5985083b5a26f2537b62815871ab
Qais-ir <qais.irshaid77@gmail.com>
2026-08-17 21:53:08 +0300

    Data Binding

--- frontend/HRMS/src/app/app.component.ts
+++ frontend/HRMS/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgIf, NgFor, NgClass, NgStyle } from '@angular/common';
+import { FormsModule, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 // Decorator
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgIf, NgFor, NgClass, NgStyle],
+  // componet, directives, module, pipe
+  imports: [RouterOutlet, NgIf, NgFor, NgClass, NgStyle, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -24,6 +26,36 @@ export class App {
     {id:4, name: "stu5", mark: 98},
     {id:5, name: "stu6", mark: 66},
   ];
+
+  images = [
+    "https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=2210&quality=70",
+    "https://t3.ftcdn.net/jpg/02/70/35/00/360_F_270350073_WO6yQAdptEnAhYKM5GuA9035wbRnVJSr.jpg",
+    "https://images.pexels.com/photos/26151151/pexels-photo-26151151/free-photo-of-night-sky-filled-with-stars-reflecting-in-the-lake.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  ];
+  currentIndex: number = 0; // Global Variable
+
+  name : string = "employee";
+
+  form = new FormGroup({
+    // Form Controls
+    name: new FormControl("Employee"),
+  });
+
+  next(){
+    // let temp = 5; // local variable
+    // temp++;
+
+    if(this.currentIndex < this.images.length - 1){
+      this.currentIndex++;
+    }
+  }
+
+  previous(){
+    if(this.currentIndex > 0){
+        this.currentIndex--;
+    }
+  }
+
 
   temp(x : number , y : string) : number{
     let num : number;
