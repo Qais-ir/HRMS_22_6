87cfbc8595f063df695566ba0d9a13cadacda74a
Qais-ir <qais.irshaid77@gmail.com>
2026-08-19 21:33:35 +0300

    Google Forms + Pipes

--- frontend/HRMS/src/app/app.component.ts
+++ frontend/HRMS/src/app/app.component.ts
@@ -1,65 +1,52 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgIf, NgFor, NgClass, NgStyle } from '@angular/common';
-import { FormsModule, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 // Decorator
 @Component({
   selector: 'app-root',
   // componet, directives, module, pipe
-  imports: [RouterOutlet, NgIf, NgFor, NgClass, NgStyle, FormsModule, ReactiveFormsModule],
+  imports: [RouterOutlet, NgIf, NgFor, NgClass, NgStyle, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class App {
 
-  title: string = "Welcome to Angular from Typescript";
-  number : number = 55.2254;
-  bool : boolean = true;
-  arr : string[] = ["one", "two", "three"];
-
-  students = [
-    {id:0, name: "stu1", mark: 49},
-    {id:1, name: "stu2", mark: 88},
-    {id:2, name: "stu3", mark: 56},
-    {id:3, name: "stu4", mark: 32},
-    {id:4, name: "stu5", mark: 98},
-    {id:5, name: "stu6", mark: 66},
-  ];
-
-  images = [
-    "https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-fall-nature-scenery-free-image.jpeg?w=2210&quality=70",
-    "https://t3.ftcdn.net/jpg/02/70/35/00/360_F_270350073_WO6yQAdptEnAhYKM5GuA9035wbRnVJSr.jpg",
-    "https://images.pexels.com/photos/26151151/pexels-photo-26151151/free-photo-of-night-sky-filled-with-stars-reflecting-in-the-lake.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-  ];
-  currentIndex: number = 0; // Global Variable
-
-  name : string = "employee";
 
   form = new FormGroup({
-    // Form Controls
-    name: new FormControl("Employee"),
+    name: new FormControl(null, Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    phone: new FormControl(null, [Validators.required, Validators.minLength(9), Validators.maxLength(10) ]),
+    age: new FormControl(null, [Validators.min(18), Validators.max(35)]),
+    courseId: new FormControl(1, Validators.required),
   });
 
-  next(){
-    // let temp = 5; // local variable
-    // temp++;
 
-    if(this.currentIndex < this.images.length - 1){
-      this.currentIndex++;
-    }
+  courses = [
+    {id : 1, name: "Asp.Net"},
+    {id : 2, name: "Angular"},
+    {id : 3, name: "Pyhton"},
+    {id : 4, name: "Java"},
+  ];
+
+  price : number = 112223.55;
+
+  date : Date = new Date();
+
+  resetForm(){
+    this.form.reset({
+      courseId: 1
+    });
   }
 
-  previous(){
-    if(this.currentIndex > 0){
-        this.currentIndex--;
-    }
+  submit(){
+    let name = this.form.value.name;
+    let courseId = this.form.value.courseId;
+    let courseName = this.courses.find(x => x.id == courseId)?.name;
+    alert(`Welcome ${name} to the academy.
+      We will contact you shortly about the ${courseName} course.`)
   }
 
-
-  temp(x : number , y : string) : number{
-    let num : number;
-    //num = "ss";
-    return 5;
-  }
 }
